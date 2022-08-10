e6ce6b1f01e99339d2e1bec4aef96145fd172003
vazgengalstyan <psyuzanna13@gmail.com>
2022-08-10 21:31:03 +0400

    inputs homework

--- script.js
+++ script.js
@@ -1,69 +1,22 @@
-//task 1 Հետեւյալ օբյեկտից նկարել ծառ html-ում, և ծառի ամեն ճյուղը ներկել կարմիր։
+// task 1
+let count = () => {
 
-let data = {
-        "Рыбы": {
-                "форель": {},
-                "лосось": {}
-        },
-
-        "Деревья": {
-                "Огромные": {
-                        "секвойя": {},
-                        "дуб": {}
-                },
-                "Цветковые": {
-                        "яблоня": {},
-                        "магнолия": {}
-                }
-        }
-};
-
-
-let crateData = data => {
-        let tree = document.createElement('ul');
-        for (let key in data) {
-                let list = document.createElement('li');
-                list.innerText = key;
-
-
-                let valueObj = crateData(data[key]);
-                if (valueObj) {
-                        list.append(valueObj);
-                        valueObj.style.color = 'red'
-                }
-
-                tree.append(list);
-        }
-
-        return tree;
+	let operator = id_2.value;
+	let number1 = id_1.value;
+	let number2 = id_3.value;
+		let result;
+	if(operator === '+'){
+		result = (Number(number1) + Number(number2))
+	}else if(operator === '-'){
+	   result = (Number(number1) - Number(number2))
+	}else if(operator === '*'){
+	result = (Number(number1) * Number(number2))
+	}else if(operator === '/'){
+	result = (Number(number1) / Number(number2))
+	}else{
+		alert('Please try again');
+	}
+	
+	alert(result)
 }
-
-
-let func = (dataBase, data) => {
-        dataBase.append(crateData(data));
-}
-let dataBase = document.getElementById('dataBase');
-func(dataBase, data);
-
-//task 2 Գրել ֆունկցիա, որը նման մասիվից html-ում կավելացնի էլեմենտներ։
-
-let elements = [
-        { element: 'p', content: 'test text', id: 1 },
-        { element: 'div', class: 'block', id: 2 },
-        { element: 'input', value: 'test'}
-]
-
-let func1 = elements => {
-        elements.forEach((item) => {
-
-                let array = document.createElement(item.element)
-                for (let key in item) {
-                        array.setAttribute(key, item[key])
-
-                }
-                document.getElementById('divId').append(array)
-
-        })
-}
-func1(elements)
 
